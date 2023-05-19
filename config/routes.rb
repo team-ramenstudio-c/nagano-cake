278474a2476e49823fbe4fa94f9c298fe73263b9
uknow1229 <86299044+uknow1229@users.noreply.github.com>
2023-05-19 08:02:36 +0000

    [Add]sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ devise_for :customers,skip: [:passwords], controllers: {
   sessions: 'public/sessions'
 }
 
+# devise_scope :customers do
+#  get 'customers/sign_out' => 'public/sessions#destroy'
+# end
+
 # 管理者用
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
