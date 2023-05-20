7a3423829bcc311832b4df5aebf524194ac30ee5
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-20 05:43:57 +0000

    [Add] カート商品表示

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,30 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
+    @cart_items = CartItem.all
   end
 
   def create
-    render 'index'
+    # binding.pry
+    @cart_item = CartItem.new(cart_item_params)
+    if cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
+      redirect_to cart_items_path
+    elsif @cart_item.save
+      @cart_items = CartItem.all
+      render 'index'
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
   end
 
 end
