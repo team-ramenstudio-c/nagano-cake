00d0538c1e5768f3c11e1ddaf4b1f5819e3f1b1b
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-20 10:17:47 +0000

    [Add]注文情報機能実装一旦終了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,23 +2,39 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = current_customer.cart_items.all
   end
 
   def create
-    # binding.pry
-    @cart_item = CartItem.new(cart_item_params)
-    if cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = cart_items.find_by(item_id: params[:cart_item][:item_id])
+    # binding.pry（gem "pry-byebug"用の機能）
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.quantity += params[:cart_item][:quantity].to_i
       cart_item.save
       redirect_to cart_items_path
     elsif @cart_item.save
-      @cart_items = CartItem.all
+      @cart_items = current_customer.cart_items.all
       render 'index'
     else
       render 'index'
     end
+  end
+
+  def update
+    @cart_item = current_customer.cart_items.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to request.referer
+  end
+
+  def destroy
+    current_customer.cart_items.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  def clear
+    current_customer.cart_items.destroy_all
+    redirect_to request.referer
   end
 
   private
