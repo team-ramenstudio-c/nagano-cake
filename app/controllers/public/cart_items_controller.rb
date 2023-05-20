af85c8ab55d1977d3d8ba930f8f771844afdb03b
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-20 17:52:32 +0000

    [Finish]カート機能全般・フラッシュメッセージ付き

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,29 +14,29 @@ class Public::CartItemsController < ApplicationController
       cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
       cart_item.quantity += params[:cart_item][:quantity].to_i
       cart_item.save
-      redirect_to cart_items_path
+      redirect_to cart_items_path, notice: 'カートに商品が追加されました'
     elsif @cart_item.save
       @cart_items = current_customer.cart_items.all
-      redirect_to cart_items_path
+      redirect_to cart_items_path, notice: 'カートに商品が追加されました'
     else
-      redirect_to request.referer
+      redirect_to request.referer, notice: 'もう一度個数を指定してください'
     end
   end
 
   def update
     @cart_item = current_customer.cart_items.find(params[:id])
     @cart_item.update(cart_item_params)
-    redirect_to request.referer
+    redirect_to request.referer, notice: '情報更新が更新されました'
   end
 
   def destroy
     current_customer.cart_items.find(params[:id]).destroy
-    redirect_to request.referer
+    redirect_to request.referer, notice: '該当商品が削除されました'
   end
 
   def clear
     current_customer.cart_items.destroy_all
-    redirect_to request.referer
+    redirect_to request.referer, notice: 'カート内が空になりました'
   end
 
   private
