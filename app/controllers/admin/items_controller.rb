1afc40edaaad9e42d5b6e907ef7e8d47eb5ff0d3
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-05-20 04:07:01 +0000

    [Add]商品登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,33 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+     redirect_to admin_item_path(@item.id)
+    else
+     render :new
+    end
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_image,:genre_id,:name,:description,
+                                 :price_without_tax,:is_on_sale)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,14 @@ class Item < ApplicationRecord
   validates :name, :description, :price_without_tax, presence: true
   validates :is_on_sale, inclusion: { in: [true, false] }
 
+  has_one_attached :item_image
+
+  def get_image
+    unless item_image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.jpg")
+      item_image.attach(io: File.open(file_path),filename:"no_image.jpg",content_type:"image/jpeg")
+    end
+    item_image
+  end
+
 end
