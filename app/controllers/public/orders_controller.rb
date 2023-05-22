c8ebb09149026080f2912940e732aa9f02092ff2
yoshimitakuto <129573171+yoshimitakuto@users.noreply.github.com>
2023-05-22 01:05:32 +0000

    [Add] 注文詳細入力完成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,16 +1,71 @@
 class Public::OrdersController < ApplicationController
+  before_action :authenticate_customer!
+
+  def new
+    @order = Order.new
+    @shipping_address = ShippingAddress.all
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @cart_items = current_customer.cart_items
+    @total_amount = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
+    @order.postage = 800
+    @order_total_amount = @total_amount + @order.postage.to_i
+
+    if params[:order][:select_address] == "0"
+      @order.shipping_post_code = current_customer.post_code
+      @order.shipping_address = current_customer.address
+      @order.shipping_name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:select_address] == "1"
+      if ShippingAddress.exists?(name: params[:order][:shipping_address_id])
+        @address = ShippingAddress.find(params[:order][:shipping_address_id])
+        @order.shipping_name = @address.name
+        @order.shipping_post_code = @address.postal_code
+        @order.shipping_address = @address.address
+      else
+       flash[:notice] = "配送先情報がありません"
+       render 'new'
+      end
+    elsif params[:order][:select_address] == "2"
+      @order.shipping_name = params[:order][:shipping_name]
+      @order.shipping_post_code = params[:order][:shipping_post_code]
+      @order.shipping_address = params[:order][:shipping_address]
+    else
+      render 'new'
+    end
+    @address = "〒" + @order.shipping_post_code + @order.shipping_address
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    if @order.save
+      current.cusomer.cart_items.each do |cart_item|
+        @order_item = OrderItem.new
+        @order_item.item_id = cart_item.item_id
+        @order_item.order_id = @order.id
+        # @order_item.price_including_tax = cart_item.
+        @order_item.quantity = cart_item.item.quantity
+        @order_item.roduction_status = 0
+      end
+    else
+    end
+  end
+
+  def completed
+  end
+
   def index
   end
 
   def show
   end
 
-  def new
-  end
+  private
 
-  def confirm
-  end
-
-  def completed
+  def order_params
+    params.require(:order).permit(:customer_id, :postage, :billing_amount, :status, :payment_method, :shipping_name, :shipping_post_code, :shipping_address)
   end
 end
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -1,8 +1,13 @@
 class ShippingAddress < ApplicationRecord
-  
+
   belongs_to :customer
-  
+
   #バリデーション設定
   validates :name, :postal_code, :address, presence: true
-  
+
+ #配送先情報を表示するためのメソッド
+  def address_display
+    "〒" + postal_code + " " + address + " " + name
+  end
+
 end
