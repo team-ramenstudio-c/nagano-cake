cc907d6531e62c5fccb10baaf1e4a952910b844c
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-05-23 06:03:31 +0000

    [Add]管理者 注文詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,16 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-  end
-  
-  def update
-   @order = Order.find(params[:id])
-   if @order.update(order_params)
-     redirect_to admin_order_path(@order.id)
-    else
-      render :show
-    end
+    @order_items =  @order.order_items
   end
 
-  
+  def update
+   @order = Order.find(params[:id])
+   @order_items = @order.order_items
+   if @order.update(order_params)
+    @order_items.update_all(production_status: "waiting_for_production") if @order.status == "confirmation_of_payment"
+   end
+    redirect_to request.referer
+  end
+
+  private
+   def order_params
+    params.require(:order).permit(:status)
+   end
+
 end
