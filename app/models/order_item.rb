64dda271f0821423984351aa5bff70e3954d29f5
NGIKK <129597259+NGIKK@users.noreply.github.com>
2023-05-24 03:02:14 +0000

    修正

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -11,7 +11,7 @@ class OrderItem < ApplicationRecord
   enum production_status: { unable_to_start: 0, waiting_for_production: 1,  in_production: 2, completion_of_production: 3 }
 
   def subtotal
-    item.with_tax_price * self.quantity
+    self.price_including_tax * self.quantity
   end
 
 end
